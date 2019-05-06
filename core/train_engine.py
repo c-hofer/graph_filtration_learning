0812f7b1b88d1083d8b8bdedcf60dae48f3bab3a
Christoph Hofer <chofer@cosy.sbg.ac.at>
2019-05-06 12:21:14 +0200

    DEV: cleanup; added meta info to exp result

--- core/train_engine.py
+++ core/train_engine.py
@@ -80,20 +80,21 @@ def experiment(exp_cfg, output_dir, device, verbose=True):
     torch.cuda.manual_seed_all(0)
 
     dataset = dataset_factory(exp_cfg['dataset_name'], verbose=verbose)
-    splits = train_test_split(dataset, verbose=verbose)
+    split_ds, split_i = train_test_split(dataset, verbose=verbose)
 
-    cv_acc = [[] for _ in range(len(splits))]
-    cv_epoch_loss = [[] for _ in range(len(splits))]
+    cv_acc = [[] for _ in range(len(split_ds))]
+    cv_epoch_loss = [[] for _ in range(len(split_ds))]
     
     output_path = osp.join(output_dir,str(uuid.uuid4()) + '.pickle')
     ret = {
         'exp_cfg': exp_cfg, 
-        'cv_acc': cv_acc, 
+        'cv_test_acc': cv_acc, 
+        'cv_indices_trn_tst': split_i,
         'cv_epoch_loss': cv_epoch_loss, 
         'start_time': datetime.datetime.now()
     }
     
-    for fold_i, (train_split, test_split) in enumerate(splits):      
+    for fold_i, (train_split, test_split) in enumerate(split_ds):      
         
 
         model = model_id_to_class[model_cfg['model_type']](
@@ -146,6 +147,16 @@ def experiment(exp_cfg, output_dir, device, verbose=True):
                 epoch_loss += loss.item()
                 opt.step()
 
+                if verbose: 
+                    print("Epoch {}/{}, Batch {}/{}".format(
+                        epoch_i, 
+                        training_cfg['num_epochs'], 
+                        batch_i, 
+                        len(dl_train)), 
+                        end='\r')
+
+            if verbose: print('')
+
             test_acc = evaluate(dl_test, model, device)
             if verbose: print("loss {:.2f} | test_acc {:.2f}".format(epoch_loss, test_acc*100.0))
             
@@ -188,103 +199,4 @@ def experiment_multi_device(exp_cfgs, output_dir, visible_devices, max_process_o
                 print(r)
                 print("# experiment configuration:")
                 print(r.exp_cfg)
-
-
-if __name__ == '__main__':
-    dataset_names = [
-        #'COLLAB',
-        #'IMDB-MULTI',
-        'IMDB-BINARY',
-        #'ENZYMES']
-        #'PTC_PGNN',
-        #'PTC_FM',
-        #'PTC_FR',
-        #'PTC_MM',
-        #'PTC_MR',
-        #'PROTEINS',
-        #'DD',
-        # 'NCI1']
-        #'MUTAG'
-    ]
-    print(dataset_names)
-
-
-    dataset_has_node_lab = {n: True if dataset_factory(n, verbose=False).num_node_lab is not None else False for n in dataset_names}
-    dataset_has_node_lab
-    
-    training_cfg = {
-        'lr': 0.01, 
-        'lr_drop_fact': 0.5, 
-        'num_epochs': 60,
-        'epoch_step': 10,
-        'batch_size': 64,
-        'weight_decay': 10e-06,
-    }
-    training_cfgs = [training_cfg]
-
-    model_cfg = {
-        'model_type': 'PershomModel',
-        'model_kwargs': {
-            'filtration_kwargs': {
-                'use_node_deg': None,
-                'use_node_lab': None,
-                'num_gin': 1,
-                'hidden_dim': 64, 
-                'use_mlp': None
-            }
-        }
-    }
-
-    model_cfgs = []
-    for a, b, c in itertools.product([True, False], [True, False], [True, False]):
-        if (not a) and (not b):
-            continue
-        
-        tmp = copy.deepcopy(model_cfg)
-        tmp['model_kwargs']['filtration_kwargs']['use_node_deg'] = a
-        tmp['model_kwargs']['filtration_kwargs']['use_node_lab'] = b
-        tmp['model_kwargs']['filtration_kwargs']['use_mlp'] = c
-        
-        model_cfgs.append(tmp)
-        
-    no_filt_learning = copy.deepcopy(model_cfg)
-    no_filt_learning['model_kwargs']['filtration_kwargs'] = None
-
-    model_cfgs.append(no_filt_learning)
-
-    exp_cfgs = []
-    for a, b, c in itertools.product(dataset_names, training_cfgs, model_cfgs):
-        
-        # filter out datasets which have no node labels
-        if not dataset_has_node_lab[a]:
-            if c['model_kwargs']['filtration_kwargs'] is not None:
-                use_node_lab = c['model_kwargs']['filtration_kwargs']['use_node_lab']
-                
-                if use_node_lab:
-                    continue
-                
-        
-        tmp = {
-            'dataset_name': a, 
-            'training': b, 
-            'model': c
-        }
-        exp_cfgs.append(tmp)
-        
-    print("# Number of configurations", len(exp_cfgs))
-
-    #output_dir = '/home/pma/chofer/repositories/nips_2019_code/results'
-    output_dir = '/tmp/test_output_dir'
-
-
-    num_device = 4
-    max_process_on_device = 2
-    manager = mp.Manager()
-    device_counter = manager.dict({t: 0 for t in range(num_device)})
-    lock = manager.Lock()
-
-    task_args = [(exp_cfg, output_dir, device_counter, lock, max_process_on_device) for exp_cfg in exp_cfgs]
-
-    with mp.Pool(num_device*max_process_on_device, maxtasksperchild=1) as pool:
-        for i, _ in enumerate(pool.imap_unordered(experiment, task_args)):
-            print("# Finished job {}/{}".format(i, len(task_args)))
+  
